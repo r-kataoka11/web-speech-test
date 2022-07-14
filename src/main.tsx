50f7e149cc0750a41d0180b5062359251476cc2e
r-kataoka11 <r.kataoka.ps.ai@gmail.com>
2022-07-14 14:54:58 +0900

    🐛 react-speech-recognitionが動かない問題を修正 https://github.com/JamesBrill/react-speech-recognition#regeneratorruntime-is-not-defined

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import 'regenerator-runtime/runtime'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
