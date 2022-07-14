e6cd1cf2580aa04fdab5536c323d9ea4a1718d3c
r-kataoka11 <r.kataoka.ps.ai@gmail.com>
2022-07-14 14:54:10 +0900

    ✨ Web Speech APIのテストプログラムを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,42 @@
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from 'react-speech-recognition'
+
 import './App.css'
 
-function App() {
+const App = () => {
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition()
+
+  console.log(browserSupportsSpeechRecognition)
+
+  if (!browserSupportsSpeechRecognition) {
+    return (
+      <div className="app">
+        <h1>Web Speech API テスト</h1>
+        <p>お使いのブラウザーはサポートしていません。</p>
+      </div>
+    )
+  }
+
   return (
     <div className="app">
       <h1>Web Speech API テスト</h1>
+      <p>マイク：{listening ? 'オン' : 'オフ'}</p>
+      <p>
+        <button onClick={() => SpeechRecognition.startListening()}>
+          スタート
+        </button>
+        <button onClick={() => SpeechRecognition.stopListening()}>
+          ストップ
+        </button>
+        <button onClick={resetTranscript}>ストップ</button>
+      </p>
+      <p>{transcript}</p>
     </div>
   )
 }
